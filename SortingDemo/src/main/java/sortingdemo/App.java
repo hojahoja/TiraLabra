67894a8f0381025e25671ee21ce3502e81b3b71f
Jari <juri@juri-HP-620.(none)>
2014-02-19 21:04:37 +0200

    MS

--- SortingDemo/src/main/java/sortingdemo/App.java
+++ SortingDemo/src/main/java/sortingdemo/App.java
@@ -1,6 +1,8 @@
 package sortingdemo;
 
+import sortingdemo.mainElem.MergeSort;
 import sortingdemo.mainElem.UI;
+import java.util.Arrays;
 
 /**
  * Hello world!
@@ -9,7 +11,15 @@ import sortingdemo.mainElem.UI;
 public class App {
 
     public static void main(String[] args) {
-        UI ui = new UI();
-        ui.start();
+//        UI ui = new UI();
+//        ui.start();
+        
+        Integer[] a = {20, 3, 1,2, 5,7};
+        MergeSort ms = new MergeSort();
+        ms.insertArray(a);
+        ms.sort();
+        
+        System.out.println(Arrays.toString(a));
+        
     }
 }
--- SortingDemo/src/main/java/sortingdemo/mainElem/MergeSort.java
+++ SortingDemo/src/main/java/sortingdemo/mainElem/MergeSort.java
@@ -1,39 +1,60 @@
 package sortingdemo.mainElem;
 
+import java.util.Arrays;
+
 /**
  * This class handles the merge sort operations.
- * 
+ *
  * @author juri
  */
 public class MergeSort {
+
     private Integer[] sortTarget;
 
     public MergeSort() {
         this.sortTarget = new Integer[0];
     }
-    
+
     public void insertArray(Integer[] sortTarget) {
         this.sortTarget = sortTarget;
     }
-    
+
     public void sort() {
-        int length = 1;
-        while (length < sortTarget.length) {            
-            int start = 1;
-            while (start + length <= sortTarget.length) {                
-                int low = start;
-                int mid = start+length;
-                int high = Math.min(mid+length, sortTarget.length);
-                merge(low, mid, high);
-                start = start + 2 * length;
-            }
-            length = 2 * length;
+        sort(1, sortTarget.length);
+    }
+    
+    public void sort( int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            sort(low, mid);
+            sort(mid+1, high);
+            merge(low, mid, high);
         }
     }
-    
+
     private void merge(int low, int mid, int high) {
-        
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+        int[] left = new int[n1+1];
+        int[] right = new int[n2+1];
+        for (int i = 0; i < n1; i++) {
+            left[i] = sortTarget[low + i-1];
+        }
+        left[n1] = Integer.MAX_VALUE / 2;
+        for (int i = 0; i < n2; i++) {
+            right[i] = sortTarget[mid + i];
+        }
+        right[n2] = Integer.MAX_VALUE / 2;
+        int i = 0;
+        int j = 0;
+        for (int k = low-1; k < high; k++) {
+            if (left[i] <= right[j]) {
+                sortTarget[k] = left[i];
+                i++;
+            } else{
+                sortTarget[k] = right[j];
+                j++;
+            }
+        }
     }
-    
-    
 }
