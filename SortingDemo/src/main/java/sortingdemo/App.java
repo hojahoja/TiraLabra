9e5ad2e3460b70b875b0aeeacfe22c4a81af9ccf
Jari <juri@juri-HP-620.(none)>
2014-02-20 05:25:20 +0200

    HS

--- SortingDemo/src/main/java/sortingdemo/App.java
+++ SortingDemo/src/main/java/sortingdemo/App.java
@@ -3,6 +3,7 @@ package sortingdemo;
 import sortingdemo.mainElem.MergeSort;
 import sortingdemo.mainElem.UI;
 import java.util.Arrays;
+import sortingdemo.mainElem.HeapSort;
 import sortingdemo.mainElem.QuickSort;
 
 /**
@@ -15,12 +16,12 @@ public class App {
 //        UI ui = new UI();
 //        ui.start();
 //        
-        Integer[] a = {100,20, 3, 1,2, 5,7, 3};
-        QuickSort qs = new QuickSort();
-        qs.insertArray(a);
-        qs.sort();
+        Integer[] a = {20, 3, 1,2, 5,7, 3};
+        HeapSort hs = new HeapSort();
+        hs.insertArray(a);
+        hs.sort();
         
-        System.out.println(Arrays.toString(qs.getSortTarget()));
+        System.out.println(Arrays.toString(hs.getSortTarget()));
         
         int i = 0;
         while (i < 10) i++;
--- SortingDemo/src/main/java/sortingdemo/mainElem/HeapSort.java
+++ SortingDemo/src/main/java/sortingdemo/mainElem/HeapSort.java
@@ -1,10 +1,71 @@
-
 package sortingdemo.mainElem;
 
 /**
  * This class handles the Heap-Sort operations.
+ *
  * @author juri
  */
 public class HeapSort {
+
+    private Integer[] sortTarget;
+    private  int heapSize;
+    private  int left;
+    private  int right;
+    private  int largest;
+
+    public HeapSort() {
+        this.sortTarget = new Integer[0];
+    }
+
+    public void insertArray(Integer[] sortTarget) {
+        this.sortTarget = sortTarget.clone();
+    }
     
+    public void sort() {
+        buildheap();
+        
+        for( int i = heapSize; i > 0 ; i--){
+            swap(0, i);
+            heapSize--;
+            heapify(0);
+        }
+    }
+    
+    private void buildheap(){
+        heapSize = sortTarget.length-1;
+        for(int i= heapSize/2 ; i >= 0; i--){
+            heapify(i);
+        }
+    }
+    
+    
+    private void heapify(int i) {
+        left=2*i;
+        right=2*i+1;
+        if(left <= heapSize && sortTarget[left] > sortTarget[i]){
+            largest=left;
+        }
+        else{
+            largest=i;
+        }
+        
+        if(right <= heapSize && sortTarget[right] > sortTarget[largest]){
+            largest=right;
+        }
+        
+        if (largest != i) {
+            swap(i, largest);
+            heapify(largest);
+        }
+    }
+    
+    private void swap(int index1, int index2) {
+        int temp = sortTarget[index1];
+        sortTarget[index1] = sortTarget[index2];
+        sortTarget[index2] = temp;
+    }
+
+    public Integer[] getSortTarget() {
+        return sortTarget;
+    }
 }
